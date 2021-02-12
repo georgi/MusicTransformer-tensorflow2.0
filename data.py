95b40f198637b0a09ea51b0edba5a622ca65cc20
Matthias Georgi <matti.georgi@gmail.com>
2021-02-12 00:24:22 +0000

    data loading from protobuf files

--- data.py
+++ data.py
@@ -4,169 +4,64 @@ import pickle
 from tensorflow.python import keras
 import numpy as np
 import params as par
+from random import randrange, gauss
+        
+def train_test_split(dataset, split=0.90):
+    train = list()
+    train_size = split * len(dataset)
+    dataset_copy = list(dataset)
+    while len(train) < train_size:
+        index = randrange(len(dataset_copy))
+        train.append(dataset_copy.pop(index))
+    return train, dataset_copy
 
 
 class Data:
-    def __init__(self, dir_path, token_eos=par.token_eos, pad_token=par.pad_token):
+    def __init__(self, sequences, token_eos, pad_token):
         self.token_eos = token_eos
         self.pad_token = pad_token
-        self.files = list(utils.find_files_by_extensions(dir_path, ['.pickle']))
-        self.file_dict = {
-            'train': self.files[:int(len(self.files) * 0.8)],
-            'eval': self.files[int(len(self.files) * 0.8): int(len(self.files) * 0.9)],
-            'test': self.files[int(len(self.files) * 0.9):],
-        }
-        self._seq_file_name_idx = 0
-        self._seq_idx = 0
-        pass
-
-    def __repr__(self):
-        return '<class Data has "'+str(len(self.files))+'" files>'
+        self.sequences = sequences
+        
+    def __len__(self):
+        return sum(len(s) for s in self.sequences.values()) 
 
     def batch(self, batch_size, length, mode='train'):
-
-        batch_files = random.sample(self.file_dict[mode], k=batch_size)
-
         batch_data = [
-            self._get_seq(file, length)
-            for file in batch_files
+            self._get_seq(seq, length, mode)
+            for seq in random.sample(self.sequences[mode], k=batch_size)
         ]
         return np.array(batch_data)  # batch_size, seq_len
-
-    def seq2seq_batch(self, batch_size, length, mode='train'):
-        data = self.batch(batch_size, length * 2, mode)
-        x = data[:, :length]
-        y = data[:, length:]
-        return x, y
-
-    def smallest_encoder_batch(self, batch_size, length, mode='train'):
-        data = self.batch(batch_size, length * 2, mode)
-        x = data[:, :length//100]
-        y = data[:, length//100:length//100+length]
-        return x, y
 
     def slide_seq2seq_batch(self, batch_size, length, mode='train'):
         data = self.batch(batch_size, length+1, mode)
         x = data[:, :-1]
         y = data[:, 1:]
         return x, y
+    
+    def augment(self, ns):
+        stretch_factor = gauss(1.0, 0.5)
+        velocity_factor = gauss(1.0, 0.2)
+        transpose = randrange(-5, 7)
+        ns = stretch_note_sequence(ns, stretch_factor)
+        for note in ns.notes:
+            note.velocity = max(1, min(127, int(note.velocity * velocity_factor)))
+        return transpose_note_sequence(ns, transpose, in_place=True)[0]
 
-    def random_sequential_batch(self, batch_size, length):
-        batch_files = random.sample(self.files, k=batch_size)
-        batch_data = []
-        for i in range(batch_size):
-            data = self._get_seq(batch_files[i])
-            for j in range(len(data) - length):
-                batch_data.append(data[j:j+length])
-                if len(batch_data) == batch_size:
-                    return batch_data
-
-    def sequential_batch(self, batch_size, length):
-        batch_data = []
-        data = self._get_seq(self.files[self._seq_file_name_idx])
-
-        while len(batch_data) < batch_size:
-            while self._seq_idx < len(data) - length:
-                batch_data.append(data[self._seq_idx: self._seq_idx + length])
-                self._seq_idx += 1
-                if len(batch_data) == batch_size:
-                    return batch_data
-
-            self._seq_idx = 0
-            self._seq_file_name_idx = self._seq_file_name_idx + 1
-            if self._seq_file_name_idx == len(self.files):
-                self._seq_file_name_idx = 0
-                print('iter intialized')
-
-    def _get_seq(self, fname, max_length=None):
-        with open(fname, 'rb') as f:
-            data = pickle.load(f)
-        if max_length is not None:
-            if max_length <= len(data):
-                start = random.randrange(0,len(data) - max_length)
-                data = data[start:start + max_length]
-            else:
-                data = np.append(data, self.token_eos)
-                while len(data) < max_length:
-                    data = np.append(data, self.pad_token)
+    def _get_seq(self, ns, max_length, mode):
+        if mode == 'train':
+            try:
+                data = encode_note_sequence(midi_encoder, self.augment(ns))
+            except BaseException as e:
+                print(e)
+                data = encode_note_sequence(midi_encoder, ns)
+        else:
+            data = encode_note_sequence(midi_encoder, ns)
+            
+        if max_length < len(data):
+            start = random.randrange(0, len(data) - max_length)
+            data = data[start:start + max_length]
+        else:
+            data = np.append(data, self.token_eos)
+            while len(data) < max_length:
+                data = np.append(data, self.pad_token)
         return data
-
-
-class PositionalY:
-    def __init__(self, data, idx):
-        self.data = data
-        self.idx = idx
-
-    def position(self):
-        return self.idx
-
-    def data(self):
-        return self.data
-
-    def __repr__(self):
-        return '<Label located in {} position.>'.format(self.idx)
-
-
-def add_noise(inputs: np.array, rate:float = 0.01): # input's dim is 2
-    seq_length = np.shape(inputs)[-1]
-
-    num_mask = int(rate * seq_length)
-    for inp in inputs:
-        rand_idx = random.sample(range(seq_length), num_mask)
-        inp[rand_idx] = random.randrange(0, self.pad_token)
-
-    return inputs
-
-
-if __name__ == '__main__':
-    import pprint
-    def count_dict(max_length, data):
-        cnt_arr = [0] * max_length
-        cnt_dict = {}
-        # print(cnt_arr)
-        for batch in data:
-            for index in batch:
-                try:
-                    cnt_arr[int(index)] += 1
-
-                except:
-                    print(index)
-                try:
-                    cnt_dict['index-'+str(index)] += 1
-                except KeyError:
-                    cnt_dict['index-'+str(index)] = 1
-        return cnt_arr
-
-    # print(add_noise(np.array([[1,2,3,3,4,5,6]]), rate=0.2))
-
-
-    # print(par.vocab_size)
-    # data = Data('dataset/processed')
-    # # ds = DataSequence('dataset/processed', 10, 2048)
-    # sample = data.seq2seq_batch(1000, 100)[0]
-    # pprint.pprint(list(sample))
-    # arr = count_dict(par.vocab_size+3,sample)
-    # pprint.pprint(
-    #     arr)
-    #
-    # from sequence import EventSeq, Event
-    #
-    # event_cnt = {
-    #     'note_on': 0,
-    #     'note_off': 0,
-    #     'velocity': 0,
-    #     'time_shift': 0
-    # }
-    # for event_index in range(len(arr)):
-    #     for event_type, feat_range in EventSeq.feat_ranges().items():
-    #
-    #         if feat_range.start <= event_index < feat_range.stop:
-    #             print(event_type+':'+str(arr[event_index])+' event cnt: '+str(event_cnt))
-    #             event_cnt[event_type] += arr[event_index]
-    #
-    # print(event_cnt)
-
-    # print(np.max(sample), np.min(sample))
-    # print([data._get_seq(file).shape for file in data.files])
-    #while True:
-    # print(ds.__getitem__(10)[1].argmax(-1))
